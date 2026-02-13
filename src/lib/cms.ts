bbdf656a0fc3e99461b0ff3211cae6a824e96ffd
Kian Kasad <kian@kasad.com>
2026-02-13 00:39:51 -0500

    fix: positive != non-negative 🤦‍♂️

--- src/lib/cms.ts
+++ src/lib/cms.ts
@@ -10,13 +10,13 @@ export const CMSSessionsResponseSchema = z.object({
     docs: z.array(z.unknown().transform((v) => v as Session)),
     hasNextPage: z.boolean(),
     hasPrevPage: z.boolean(),
-    limit: z.number().positive(),
+    limit: z.number().nonnegative(),
     nextPage: z.number().nullable(),
-    page: z.number().positive(),
-    pagingCounter: z.number().positive(),
+    page: z.number().nonnegative(),
+    pagingCounter: z.number().nonnegative(),
     prevPage: z.number().nullable(),
-    totalDocs: z.number().positive(),
-    totalPages: z.number().positive(),
+    totalDocs: z.number().nonnegative(),
+    totalPages: z.number().nonnegative(),
 });
 
 export const SessionsAPIResponseSchema = CMSSessionsResponseSchema.extend({
